f01b290bcf7769a43456198fb12be9561ab7062a
Raphael <73400577+werwolfy@users.noreply.github.com>
2024-10-05 11:42:32 +0200

    [TASK] Worked on simple Builder

--- TeamworkAutoTeilEins/Builder/VehicleBuilder.cs
+++ TeamworkAutoTeilEins/Builder/VehicleBuilder.cs
@@ -1,14 +1,41 @@
-﻿using TeamworkAutoTeilEins.Strategy;
+﻿
+
+using TeamworkAutoTeilEins.Strategy;
 
 namespace TeamworkAutoTeilEins.Builder;
 
 public class VehicleBuilder
 {
-    private readonly AVehicle _vehicle;
+    private AVehicle _vehicle;
 
     public VehicleBuilder SetSpeed(int speed)
     {
         _vehicle.Speed = speed;
         return this;
     }
+
+    public VehicleBuilder SetModel(string model)
+    {
+        _vehicle.Model = model;
+        return this;
+    }
+
+    public VehicleBuilder SetCurrentState(ICarState state)
+    {
+        _vehicle.CurrentState = state;
+        return this;
+    }
+
+    public VehicleBuilder SetDriveMode(IDriveStrategy strategy)
+    {
+        _vehicle.DriveMode = strategy;
+        return this;
+    }
+
+    public AVehicle BuildCar()
+    {
+        _vehicle = new Car();
+        return _vehicle;
+    }
+    
 }
